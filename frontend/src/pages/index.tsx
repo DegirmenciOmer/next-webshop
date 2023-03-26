9a128991fbdadf065f78e0baaa8e054e354f676f
DegirmenciOmer <rumidegirmenci0@gmail.com>
2023-03-26 15:49:16 +0200

    add product details page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,23 @@
 import { Inter } from 'next/font/google'
 import Layout from '@/components/Layout'
+import { ProductItem } from '@/components/ProductItem'
+import data from '@/utils/data'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <Layout>Hello world</Layout>
+  return (
+    <Layout>
+      <h2 className='h2 my-4'>Latest Products</h2>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
+        {data.products.map((product) => (
+          <ProductItem
+            product={product}
+            key={product.slug}
+            //addToCartHandler={addToCartHandler}
+          ></ProductItem>
+        ))}
+      </div>
+    </Layout>
+  )
 }
