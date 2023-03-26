0e092550b01eeed95e35a8d1601e3e4c4f085ad2
DegirmenciOmer <rumidegirmenci0@gmail.com>
2023-03-26 17:34:42 +0200

    add store context

---
+++ frontend/src/context/StoreContext.tsx
@@ -0,0 +1,61 @@
+import { Tproduct } from '@/utils/data'
+import { CART_ADD_ITEM } from '@/utils/variables'
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useReducer,
+} from 'react'
+
+export interface TCart {
+  cart: { cartItems: any[] }
+}
+
+interface TStoreContextValue {
+  cart: TCart
+  addToCart: (item: any) => void
+  removeItemFromCart: (item: any) => void
+  clearCart: () => void
+  setItemCountAndTotalPrice: (id: number, count: number) => void
+}
+
+const initialValue: TCart = { cart: { cartItems: [] } }
+
+const StoreContext = createContext<any>(initialValue)
+
+export const useStoreContext = () => useContext(StoreContext)
+
+function reducer(state: any, action: any) {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const newItem = action.payload
+      const foundItem = state.cart.cartItems.find(
+        (item: Tproduct) => item.slug === newItem.slug
+      )
+      const cartItems = foundItem
+        ? state.cart.cartItems.map((item: Tproduct) =>
+            item.name === foundItem.name ? newItem : item
+          )
+        : [...state.cart.cartItems, newItem]
+      return { ...state, cart: { ...state.cart, cartItems } }
+    default:
+      return state
+  }
+}
+
+export const StoreProvider = ({ children }: { children: React.ReactNode }) => {
+  const [state, dispatch] = useReducer(reducer, initialValue)
+
+  const value = { state, dispatch }
+
+  return (
+    <StoreContext.Provider
+      value={{
+        value,
+      }}
+    >
+      {children}
+    </StoreContext.Provider>
+  )
+}
